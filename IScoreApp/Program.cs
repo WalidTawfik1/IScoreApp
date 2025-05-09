bc155542acdf16be8ff686398a39337dc12115e8
Walid Tawfik <walidtawfik362@gmail.com>
2025-05-09 23:22:10 +0300

    Deployed to MonsterASP

--- IScoreApp/Program.cs
+++ IScoreApp/Program.cs
@@ -18,11 +18,11 @@ namespace IScoreApp
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            /*if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
-            }
+            }*/
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
